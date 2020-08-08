acfdd212903605d9fdba006894defe4606af72a3
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-08-07 23:46:30 -0400

    start schedule display

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("blorp")
+document.addEventListener('DOMContentLoaded', () => {
+    makeWeek()
+})
+
+function makeWeek(){
+    const week = document.createElement('div')
+    let d = new Date()
+    week.innerHTML = `
+        <table>
+            <tr>
+                <th>${d.toDateString()}</th>
+                <th>${d.toDateString()}</th>
+            </tr>
+            <tr>
+            </tr>
+        </table>
+    `
+    document.querySelector('body').appendChild(week)
+}
